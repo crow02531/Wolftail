0a91783a1091e90258e69757a035c0716f9c458f
crow02531 <19226347+crow02531@users.noreply.github.com>
2022-07-09 12:52:12 +0800

    optimize

--- wolftail-1.12.2/src/main/java/net/wolftail/impl/mixin/MixinPlayerList.java
+++ wolftail-1.12.2/src/main/java/net/wolftail/impl/mixin/MixinPlayerList.java
@@ -1,19 +1,16 @@
 package net.wolftail.impl.mixin;
 
-import java.net.SocketAddress;
+import java.util.List;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-import com.mojang.authlib.GameProfile;
-
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.PlayerList;
-import net.wolftail.impl.ImplMPCRoot;
 import net.wolftail.impl.SharedImpls;
 
 //'The server is full!'
@@ -24,16 +21,8 @@ public abstract class MixinPlayerList {
 	@Shadow
 	private MinecraftServer mcServer;
 	
-	@Shadow
-	public abstract boolean bypassesPlayerLimit(GameProfile profile);
-	
-	@Inject(method = "allowUserToConnect", at = @At("RETURN"))
-	private void onAllowUserToConnect(SocketAddress address, GameProfile profile, CallbackInfoReturnable<String> info) {
-		if(info.getReturnValue() == null) {
-			ImplMPCRoot rm = SharedImpls.as(this.mcServer).wolftail_getRootManager();
-			
-			if(rm.currentLoad() >= rm.maxLoad() && !this.bypassesPlayerLimit(profile))
-				info.setReturnValue("The server is full!");
-		}
+	@Redirect(method = "allowUserToConnect", at = @At(value = "INVOKE", target = "size()I"))
+	private int sizeProxy(List<EntityPlayerMP> list) {
+		return SharedImpls.as(this.mcServer).wolftail_getRootManager().currentLoad();
 	}
 }
