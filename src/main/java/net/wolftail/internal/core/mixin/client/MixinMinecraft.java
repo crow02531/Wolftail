eca8960362236580645ee725dd6e241eb1808a93
crow02531 <2192749414@qq.com>
2022-10-01 18:41:55 +0800

    directly draw in the default framebuffer

--- src/main/java/net/wolftail/internal/core/mixin/client/MixinMinecraft.java
+++ src/main/java/net/wolftail/internal/core/mixin/client/MixinMinecraft.java
@@ -24,9 +24,9 @@ import net.minecraft.client.gui.GuiDisconnected;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiMultiplayer;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.settings.GameSettings;
-import net.minecraft.client.shader.Framebuffer;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.profiler.Profiler;
 import net.minecraft.profiler.Snooper;
@@ -47,243 +47,238 @@ import net.wolftail.internal.core.network.NptPacketListener;
 //inject play context; intercept game loop for non player type
 @Mixin(Minecraft.class)
 public abstract class MixinMinecraft implements ExtCoreMinecraft {
-	
+
 	@Final
 	@Shadow
 	public static Logger LOGGER;
-	
+
 	@Final
 	@Shadow
 	public Profiler mcProfiler;
-	
+
 	@Final
 	@Shadow
 	public FrameTimer frameTimer;
-	
+
 	@Shadow
 	public GuiScreen currentScreen;
-	
+
 	@Shadow
 	public NetworkManager myNetworkManager;
-	
+
 	@Final
 	@Shadow
 	public Timer timer;
-	
+
 	@Final
 	@Shadow
 	public Queue<FutureTask<?>> scheduledTasks;
-	
+
 	@Final
 	@Shadow
 	public Snooper usageSnooper;
-	
+
 	@Shadow
 	public static int debugFPS;
-	
+
 	@Shadow
 	public String debug;
-	
+
 	@Shadow
 	public int fpsCounter;
-	
+
 	@Shadow
 	public long debugUpdateTime;
-	
+
 	@Shadow
 	public long startNanoTime;
-	
-	@Shadow
-	public Framebuffer framebufferMc;
-	
+
 	@Shadow
 	public GameSettings gameSettings;
-	
+
 	@Shadow
 	public int displayWidth;
-	
+
 	@Shadow
 	public int displayHeight;
-	
+
 	@Shadow
 	public TextureManager renderEngine;
-	
+
 	@Shadow
 	public SoundHandler mcSoundHandler;
-	
+
 	@Shadow
 	public MusicTicker mcMusicTicker;
-	
+
 	@Shadow
 	public abstract void shutdown();
-	
+
 	@Shadow
 	public abstract void updateDisplay();
-	
+
 	@Shadow
 	public abstract void displayGuiScreen(GuiScreen screen);
-	
+
 	@Shadow
 	public abstract void checkGLError(String message);
-	
+
 	// ---------------------------------SHADOW END---------------------------------
-	
+
 	@Unique
 	private static final Logger logger = LogManager.getLogger("Wolftail/User");
-	
+
 	@Unique
 	private FutureTask<Void> loadContextTask;
-	
+
 	@Unique
 	private ImplPCC playContext;
-	
+
 	@Inject(method = "init", at = @At("RETURN"))
 	private void on_init_return(CallbackInfo ci) {
 		SectionHandler.finish_loading(false);
 	}
-	
+
 	@Inject(method = "runTick", at = @At(value = "INVOKE_STRING", target = "net.minecraft.profiler.Profiler.endStartSection(Ljava/lang/String;)V", args = {
 			"ldc=textures" }))
 	private void on_runTick_invoke_endStartSection_0(CallbackInfo ci) {
 		ImplPCC context = this.playContext;
-		
+
 		if (context != null && !context.isConnected())
 			this.unloadContext(); // steve quit game
 	}
-	
+
 	@Inject(method = "run", at = @At(value = "INVOKE", target = "net.minecraft.client.Minecraft.displayGuiScreen(Lnet/minecraft/client/gui/GuiScreen;)V"))
 	private void on_run_invoke_displayGuiScreen(CallbackInfo ci) {
 		this.unloadContext();
 	}
-	
+
 	@Inject(method = "runGameLoop", at = @At("HEAD"), cancellable = true)
 	private void on_runGameLoop_head(CallbackInfo ci) {
 		FutureTask<Void> task = this.loadContextTask;
 		if (task != null) {
 			task.run();
 			rethrowException(task);
-			
+
 			this.loadContextTask = null;
 		}
-		
+
 		ImplPCC context = this.playContext;
-		
+
 		if (context != null && !context.playType().isSteve()) {
 			ci.cancel();
-			
+
 			this.doGameLoop(context);
 		}
 	}
-	
+
 	@Unique
 	private void doGameLoop(ImplPCC context) {
 		Profiler profiler = this.mcProfiler;
 		profiler.startSection("root");
-		
+
 		// check close
 		if (Display.isCreated() && Display.isCloseRequested())
 			this.shutdown();
-		
+
 		// update timer
 		this.timer.updateTimer();
 		int i = Math.min(10, this.timer.elapsedTicks);
-		
+
 		profiler.startSection("scheduledExecutables");
 		runQueuedTasks(this.scheduledTasks); // here we will apply all packets received
 		profiler.endSection();
-		
+
 		boolean lostContext = false;
-		
+
 		// ---------------------------------Tick Start---------------------------------
 		profiler.startSection("tick");
-		
+
 		// ticking part, notice that the code below dosen't always run every game loop
 		for (; i-- != 0;) {
 			NetworkManager connect = context.getConnection();
-			
+
 			if (connect.isChannelOpen())
 				connect.processReceivedPackets();
-			
+
 			if (!connect.isChannelOpen()) {
 				connect.checkDisconnected();
-				
+
 				context.playType().callClientLeave();
 				this.unloadContext(); // non player type quit game
 				this.displayQuitScreen(connect.getExitMessage(), connect.isLocalChannel());
-				
+
 				lostContext = true;
 				break;
 			}
-			
+
 			this.renderEngine.tick();
 			this.mcMusicTicker.update();
 			this.mcSoundHandler.update();
 		}
-		
+
 		profiler.endSection();
 		// ----------------------------------Tick End----------------------------------
-		
+
 		this.checkGLError("Pre render");
-		
+
 		// --------------------------------Render Start--------------------------------
 		profiler.startSection("render");
-		
-		this.framebufferMc.bindFramebuffer(true); // bind framebuffer & set viewport
-		
+
+		// directly draw to the default framebuffer
+		GlStateManager.viewport(0, 0, this.displayWidth, this.displayHeight);
+
 		// do frame
 		if (!lostContext)
 			context.playType().callClientFrame();
-		
-		this.framebufferMc.unbindFramebuffer(); // unbind framebuffer
-		this.framebufferMc.framebufferRender(this.displayWidth, this.displayHeight); // blit to screen
-		
+
 		profiler.endSection();
 		// ---------------------------------Render End---------------------------------
-		
+
 		this.updateDisplay();
 		Thread.yield();
-		
+
 		this.checkGLError("Post render");
-		
+
 		this.fpsCounter++; // call this every game loop
 		this.updateFrameTimer();
-		
+
 		while (Minecraft.getSystemTime() >= this.debugUpdateTime + 1000L) {
 			debugFPS = this.fpsCounter;
 			this.debug = String.format("Wolftail working with %s, %d fps", context.playType().getRegistryName(),
 					debugFPS);
-			
+
 			this.debugUpdateTime += 1000L;
 			this.fpsCounter = 0; // when 1s has passed, clear it
-			
+
 			this.usageSnooper.addMemoryStatsToSnooper();
 			this.usageSnooper.startSnooper(); // this will check isSnooperRunning()Z itself
 		}
-		
+
 		if ((i = this.gameSettings.limitFramerate) < GameSettings.Options.FRAMERATE_LIMIT.getValueMax()) {
 			profiler.startSection("fpslimit_wait");
 			Display.sync(i);
 			profiler.endSection();
 		}
-		
+
 		profiler.endSection(); // root
 	}
-	
+
 	@Unique
 	private void updateFrameTimer() {
 		long k = System.nanoTime();
 		this.frameTimer.addFrame(k - this.startNanoTime);
 		this.startNanoTime = k;
 	}
-	
+
 	@Unique
 	private void displayQuitScreen(ITextComponent reason, boolean isLocal) {
 		GuiMainMenu main = new GuiMainMenu();
 		GuiScreen s = isLocal ? main : new GuiMultiplayer(main);
-		
+
 		this.displayGuiScreen(reason == null ? s : new GuiDisconnected(s, "disconnect.lost", reason));
 	}
-	
+
 	@Unique
 	private static void runQueuedTasks(Queue<FutureTask<?>> the_queue) {
 		synchronized (the_queue) {
@@ -291,7 +286,7 @@ public abstract class MixinMinecraft implements ExtCoreMinecraft {
 				Util.runTask(the_queue.poll(), LOGGER);
 		}
 	}
-	
+
 	@Unique
 	private static void rethrowException(FutureTask<?> task) {
 		try {
@@ -300,23 +295,23 @@ public abstract class MixinMinecraft implements ExtCoreMinecraft {
 			Throwables.rethrow(e.getCause());
 		}
 	}
-	
+
 	@Unique
 	private void unloadContext() {
 		SectionHandler.on_client_playing_change();
-		
+
 		this.playContext = null;
 	}
-	
+
 	@Override
 	public void wolftail_loadContext(ImplUPT type, UUID id, String name, NetworkManager connect) {
 		try {
 			(this.loadContextTask = new FutureTask<Void>(() -> {
 				ImplPCC context = playContext = new ImplPCC(type, id, name, connect);
 				SectionHandler.on_client_playing_change();
-				
+
 				logger.info("The universal player type in use is {}", type.getRegistryName());
-				
+
 				if (!type.isSteve()) {
 					// clean up
 					currentScreen.onGuiClosed();
@@ -325,7 +320,7 @@ public abstract class MixinMinecraft implements ExtCoreMinecraft {
 					FMLClientHandler.instance().setPlayClient(null);
 					NptPacketListener.cleanFML(connect);
 					connect.setNetHandler(new NptClientPacketListener(connect));
-					
+
 					type.callClientEnter(context);
 				}
 			}, null)).get();
@@ -333,7 +328,7 @@ public abstract class MixinMinecraft implements ExtCoreMinecraft {
 			// never happens
 		}
 	}
-	
+
 	@Override
 	public ImplPC wolftail_currentContext() {
 		return this.playContext;
--- src/test/java/com/example/examplemod/PigClientHandler.java
+++ src/test/java/com/example/examplemod/PigClientHandler.java
@@ -1,25 +1,23 @@
 package com.example.examplemod;
 
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.Project;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.multiplayer.PlayerControllerMP;
 import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderGlobal;
-import net.minecraft.client.renderer.RenderHelper;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.culling.ClippingHelperImpl;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.culling.ICamera;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
-import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.ChatType;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.WorldSettings;
@@ -49,11 +47,16 @@ public final class PigClientHandler implements IClientHandler, INetworkHandler {
 
 		Minecraft mc = Minecraft.getMinecraft();
 
-		this.world = mc.world = new WorldClient(null, new WorldSettings(0, null, false, false, WorldType.DEFAULT), 0, null, mc.mcProfiler);
+		this.world = mc.world = new WorldClient(null, new WorldSettings(0, null, false, false, WorldType.DEFAULT), 0,
+				null, mc.mcProfiler);
 		mc.renderGlobal.setWorldAndLoadRenderers(this.world);
 		mc.effectRenderer.clearEffects(this.world);
 		TileEntityRendererDispatcher.instance.setWorld(this.world);
 		MinecraftForgeClient.clearRenderCache();
+
+		mc.player = new EntityPlayerSP(mc, mc.world, new NetHandlerPlayClient(mc, null, null, mc.getSession().getProfile()), null, null);
+		mc.playerController = new PlayerControllerMP(mc, mc.player.connection);
+		mc.setRenderViewEntity(mc.player);
 
 		for (int x = -1; x <= 1; ++x)
 			for (int z = -1; z <= 1; ++z) {
@@ -73,26 +76,21 @@ public final class PigClientHandler implements IClientHandler, INetworkHandler {
 		j = Math.max(j, 60);
 		long l = Math.max((long) (1000000000 / j / 4), 0L);
 
-		GlStateManager.viewport(0, 0, mc.displayWidth, mc.displayHeight);
-		GlStateManager.clear(16640);
+		GlStateManager.clearColor(0, 0, 0, 0);
+		GlStateManager.clearDepth(1);
+		GlStateManager.clear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
 		renderWorld(mc, mc.renderGlobal, mc.getRenderPartialTicks(), System.nanoTime() + l);
 	}
 
-	private static int frameCount;
-
 	private static void renderWorld(Minecraft mc, RenderGlobal rg, float partialTicks, long finishTimeNano) {
 		GlStateManager.enableDepth();
-		GlStateManager.enableAlpha();
-		GlStateManager.alphaFunc(516, 0.5F);
-		GlStateManager.enableCull();
-		GlStateManager.shadeModel(7425);
-		ClippingHelperImpl.getInstance(); // trigger init
-
+        GlStateManager.enableBlend();
+		
 		double x = 0;
 		double y = 5;
 		double z = 0;
-		double yaw = 0;
+		double yaw = 90;
 		double pitch = 0;
 		double roll = 0;
 
@@ -112,75 +110,29 @@ public final class PigClientHandler implements IClientHandler, INetworkHandler {
 
 		ICamera icamera = new Frustum();
 		icamera.setPosition(x, y, z);
-		Entity fakeViewEntity = makeFake(x, y, z, yaw, pitch);
+		mc.player.setPositionAndRotation(x, y, z, (float) yaw, (float) pitch);
 
-		mc.getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-		RenderHelper.disableStandardItemLighting();
+		// draw sky
+		{
+			mc.world.setWorldTime(System.currentTimeMillis() % 24000);
 
-		rg.setupTerrain(fakeViewEntity, partialTicks, icamera, frameCount++, true);
-		rg.updateChunks(finishTimeNano);
-
-		GlStateManager.matrixMode(5888);
-		GlStateManager.pushMatrix();
-
-		// draw terrain 1
-		GlStateManager.disableAlpha();
-		rg.renderBlockLayer(BlockRenderLayer.SOLID, partialTicks, 2, fakeViewEntity);
-		GlStateManager.enableAlpha();
-		mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false,
-				mc.gameSettings.mipmapLevels > 0);
-		rg.renderBlockLayer(BlockRenderLayer.CUTOUT_MIPPED, partialTicks, 2, fakeViewEntity);
-		mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
-		mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
-		rg.renderBlockLayer(BlockRenderLayer.CUTOUT, partialTicks, 2, fakeViewEntity);
-		mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
-
-		GlStateManager.matrixMode(5888);
-		GlStateManager.popMatrix();
-
-		GlStateManager.shadeModel(7424);
-		GlStateManager.alphaFunc(516, 0.1F);
-
-		// draw block breaking progress
-		GlStateManager.enableBlend();
-		GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE,
-				GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-		mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
-		rg.drawBlockDamageTexture(Tessellator.getInstance(), Tessellator.getInstance().getBuffer(), fakeViewEntity,
-				partialTicks);
-		mc.getTextureManager().getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
-
-		GlStateManager.depthMask(false);
-		GlStateManager.shadeModel(7425);
-
-		// draw terrain 2
-		mc.getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-		rg.renderBlockLayer(BlockRenderLayer.TRANSLUCENT, partialTicks, 2, fakeViewEntity);
-	}
-
-	private static Entity makeFake(double x, double y, double z, double yaw, double pitch) {
-		Entity e = new Entity(null) {
-
-			@Override
-			protected void entityInit() {
-			}
-
-			@Override
-			protected void readEntityFromNBT(NBTTagCompound compound) {
-			}
-
-			@Override
-			protected void writeEntityToNBT(NBTTagCompound compound) {
-			}
-		};
-
-		e.posX = x;
-		e.posY = y;
-		e.posZ = z;
-		e.rotationYaw = (float) yaw;
-		e.rotationPitch = (float) pitch;
-
-		return e;
+			Vec3d skyColor = mc.world.getSkyColor(mc.getRenderViewEntity(), partialTicks);
+			GlStateManager.clearColor((float) skyColor.x, (float) skyColor.y, (float) skyColor.z, 0);
+			GlStateManager.clear(GL11.GL_COLOR_BUFFER_BIT);
+			
+			GlStateManager.matrixMode(5889);
+            GlStateManager.loadIdentity();
+            Project.gluPerspective(90, (float) mc.displayWidth / (float) mc.displayHeight, 0.05F, (float) (mc.gameSettings.renderDistanceChunks * 16) * 2.0F);
+            GlStateManager.matrixMode(5888);
+			
+            rg.renderSky(partialTicks, 2);
+			
+            GlStateManager.matrixMode(5889);
+            GlStateManager.loadIdentity();
+            Project.gluPerspective(90, (float) mc.displayWidth / (float) mc.displayHeight, 0.05F,
+					(float) (mc.gameSettings.renderDistanceChunks * 16) * MathHelper.SQRT_2);
+            GlStateManager.matrixMode(5888);
+		}
 	}
 
 	@Override
@@ -203,6 +155,6 @@ public final class PigClientHandler implements IClientHandler, INetworkHandler {
 
 	@Override
 	public void tick() {
-
+		this.world.tick();
 	}
 }
